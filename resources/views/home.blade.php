8ffe63e780058e7402776daa4c6c056fedc7dc06
Roberto Ramos <it.robramos@gmail.com>
2021-03-15 14:34:17 -0600

    Se eliminan tools de dividendos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -162,13 +162,7 @@
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Dividendos</h3>
-                
-                                <div class="card-tools">
-                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                                </button>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
-                                </div>
-                            </div>
+                            </div>           
                             <div class="card-body" style="display: block;">
                                 <div class="chart"><div class="chartjs-size-monitor"><div class="chartjs-size-monitor-expand"><div class=""></div></div><div class="chartjs-size-monitor-shrink"><div class=""></div></div></div>
                                 <canvas id="barChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%; display: block; width: 604px;" width="604" height="250" class="chartjs-render-monitor"></canvas>
