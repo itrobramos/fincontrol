f15fa5330b16c323f429429ac8c062ceca526975
Roberto Ramos <it.robramos@gmail.com>
2021-03-16 22:16:45 -0600

    Redirect home a  dashboard

--- resources/views/accounts/register.blade.php
+++ resources/views/accounts/register.blade.php
@@ -14,7 +14,7 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{url('home')}}">Home</a></li>
                             <li class="breadcrumb-item"><a href="{{url('accounts')}}">Cuentas</a></li>
                             <li class="breadcrumb-item"><a href="{{url('accounts/' .$account->id)}}">{{$account->name}}</a></li>
                             <li class="breadcrumb-item active">Registrar movimiento</li>
--- resources/views/rentafija/add.blade.php
+++ resources/views/rentafija/add.blade.php
@@ -14,7 +14,7 @@
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
+                            <li class="breadcrumb-item"><a href="{{url('home')}}">Home</a></li>
                             <li class="breadcrumb-item"><a href="{{url('rentafija')}}">Renta Fija</a></li>
                             <li class="breadcrumb-item active">{{$platform->name}}</li>
                         </ol>
