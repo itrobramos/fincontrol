e061c9ca41dfa0800b7d602a361c6cb327bdd266
Roberto Ramos <it.robramos@gmail.com>
2021-05-26 23:28:57 -0500

    Imagenes de fintech

--- resources/views/fintech/index.blade.php
+++ resources/views/fintech/index.blade.php
@@ -49,7 +49,88 @@
                               <a href="redgirasol" class="btn btn-warning btn-block sticky-top"><b>Entrar</b></a>
                             </div>
                         </div>
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-dark card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" src="{{env('DEPLOY_URL')}}/images/expansive.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="expansive" class="btn btn-dark btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-danger card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" src="{{env('DEPLOY_URL')}}/images/100ladrillos.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="100ladrillos" class="btn btn-danger btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-primary card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" src="{{env('DEPLOY_URL')}}/images/monific.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="monific" class="btn btn-primary btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
     
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-danger card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" style="height: 100px;" src="{{env('DEPLOY_URL')}}/images/lendera.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="lendera" class="btn btn-danger btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-success card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" src="{{env('DEPLOY_URL')}}/images/cumplo.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="cumplo" class="btn btn-success btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-success card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" style="height:120px;" src="{{env('DEPLOY_URL')}}/images/yotepresto.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="yotepresto" class="btn btn-success btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+
+                        <div class="col-xlg-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="card card-success card-outline" style="height: 200px;">
+                              <div class="card-body box-profile">
+                                <div class="text-center">
+                                  <img class="img-fluid" style="height:120px;" src="{{env('DEPLOY_URL')}}/images/prestadero.png" alt="Logo">
+                                </div>
+                              </div>
+                              <a href="prestadero" class="btn btn-success btn-block sticky-top"><b>Entrar</b></a>
+                            </div>
+                        </div>
+
+
+
                     </div>
 
                     
